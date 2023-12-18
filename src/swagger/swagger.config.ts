a9affa9bfa89279b4e2b79f7cff864bcca640946
favour@swiftcoretech.com <favour@swiftcoretech.com>
2023-12-18 20:13:31 +0100

    swagger

--- src/swagger/swagger.config.ts
+++ src/swagger/swagger.config.ts
@@ -12,5 +12,5 @@ export function swaggerConfig(app: INestApplication): void{
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); 
+  SwaggerModule.setup('/', app, document); 
 }
