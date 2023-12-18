3cb63a1ed5baf1996092e073e658aa9109782053
favour@swiftcoretech.com <favour@swiftcoretech.com>
2023-12-18 06:28:00 +0100

    s

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
-import { RedisRepository } from 'src/redis/redis.repository';
 import { redisModule } from 'src/redis/modules.config';
 import { MulterModule } from '@nestjs/platform-express';
 import { MenuModule } from './menu/menu.module';
+import { RedisRepository } from 'src/redis/redis.repository';
 @Module({
   providers: [
     RestaurantService, 
-    // RedisRepository
+    RedisRepository
   ],
-  // imports:[redisModule],
+  imports:[
+    redisModule,
+    MulterModule.register({ dest: '../../uploads/menu-category' }),
+    MulterModule.register({ dest: '../../uploads/menu-subcategory' }),
+    MulterModule.register({ dest: '../../uploads/menu' }),
+    MenuModule,
+    redisModule
+  ],
   controllers: [RestaurantController],
 })
 export class RestaurantModule {}
