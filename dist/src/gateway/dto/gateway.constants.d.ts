06aad51cc97d364e2fb5508fcf2c52bd4203a6c9
favour@swiftcoretech.com <favour@swiftcoretech.com>
2023-12-20 16:40:07 +0100

    df

--- dist/src/gateway/dto/gateway.constants.d.ts
+++ dist/src/gateway/dto/gateway.constants.d.ts
@@ -9,3 +9,8 @@ export declare enum UserManagementEvents {
     get_all_drivers_event = "get_all_drivers_event",
     get_all_customers_event = "get_all_customers_event"
 }
+export declare enum MenuManagementEvents {
+    get_restaurant_menu_categories_event = "get_restaurant_menu_categories_event",
+    get_restaurant_menu_sub_category_event = "get_restaurant_menu_categories_event",
+    get_restaurant_menu_event = "get_restaurant_menu_event"
+}
