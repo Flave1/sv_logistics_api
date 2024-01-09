1af1f9e07b84758917a090f5d30ab238e3687d7d
favour@swiftcoretech.com <favour@swiftcoretech.com>
2024-01-09 11:05:59 +0100

    add address

--- src/restaurant/user/user.module.ts
+++ src/restaurant/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { GatewayService } from 'src/gateway/gateway.service';
+import { AddressModule } from './address/address.module';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, GatewayService]
+  providers: [UserService, GatewayService],
+  imports: [AddressModule]
 })
 export class UserModule {}
