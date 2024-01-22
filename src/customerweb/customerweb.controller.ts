e8940e87c99a0c294f7028d19a6a658261d57188
favour@swiftcoretech.com <favour@swiftcoretech.com>
2024-01-22 16:18:10 +0100

    as

--- src/customerweb/customerweb.controller.ts
+++ src/customerweb/customerweb.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CustomerWebService } from './customerweb.service';
 import { JwtGuard } from 'src/auth/guard';
 import { getBaseUrl } from 'src/utils';
 import { Request } from 'express';
 import { CustomerMobileService } from 'src/customer-mobile/customer-mobile.service';
+import { CheckoutFinalMenuRequest, RemoveMenuOrderDto, SaveMenuOrderDto } from 'src/customer-mobile/dto';
 
 
 @ApiTags('CustomerWeb')
@@ -51,5 +52,26 @@ export class CustomerWebController {
     @Get('all-menu/:restaurantId')
     async getRestaurantAllMenu(@Param('restaurantId') restaurantId: string, @Req() req: Request) {
         return this.customerWebService.getRestaurantAllMenu(restaurantId, req);
+    }
+
+    @Post('save-to-cart')
+    addToCart(@Body() dto: SaveMenuOrderDto) {
+      return this.customerMobileService.addToCartOrUpdate(dto);
+    }
+    @Post('remove-from-cart')
+    deleteFromCart(@Body() dto: RemoveMenuOrderDto) {
+      return this.customerMobileService.removefromCart(dto);
+    }
+  
+    @Get('cart-list')
+    async getCartList(@Req() req: Request, @Query('customerId') customerId?: string, @Query('temporalId') temporalId?: string) {
+      const response = await this.customerMobileService.getFromCart(req, parseInt(customerId), temporalId);
+      return response;
+    }
+  
+    @Post('get-checkout-final-menu')
+    async getCheckoutFinalmenu(@Req() req: Request, @Body() menuRequest: CheckoutFinalMenuRequest) {
+      const response = await this.customerMobileService.getCheckoutMenu(menuRequest.restaurantIds, menuRequest.menuIds, req);
+      return response;
     }
 }
